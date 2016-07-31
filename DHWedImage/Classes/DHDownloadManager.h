28d1856ff68935bd7a055971e04c0e7357c5162c
dongHao <807984698@qq.com>
2016-07-31 20:00:45 +0800

    3. 程序数据apps.json

---
+++ DHWedImage/Classes/DHDownloadManager.h
@@ -0,0 +1,13 @@
+//
+//  DHDownloadManager.h
+//  DHWedImage
+//
+//  Created by donghao on 16/7/31.
+//  Copyright © 2016年 dongHao. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DHDownloadManager : NSObject
+
+@end
---
+++ DHWedImage/Classes/DHDownloadManager.m
@@ -0,0 +1,13 @@
+//
+//  DHDownloadManager.m
+//  DHWedImage
+//
+//  Created by donghao on 16/7/31.
+//  Copyright © 2016年 dongHao. All rights reserved.
+//
+
+#import "DHDownloadManager.h"
+
+@implementation DHDownloadManager
+
+@end
