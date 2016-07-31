27c7ed1e57612bf51c0130b3e5ebdcd2b64a76b4
dongHao <807984698@qq.com>
2016-07-31 21:25:45 +0800

    6. 管理类功能实现

--- DHWedImage/Classes/DHDownloadManager.m
+++ DHWedImage/Classes/DHDownloadManager.m
@@ -7,6 +7,23 @@
 //
 
 #import "DHDownloadManager.h"
+#import "DHDownloadOperation.h"
+
+@interface DHDownloadManager ()
+/**
+ *  图片缓存
+ */
+@property(nonatomic,strong) NSMutableDictionary *imageCache;
+/**
+ *  操作缓存
+ */
+@property(nonatomic,strong) NSMutableDictionary *operationCache;
+/**
+ *  队列
+ */
+@property(nonatomic,strong) NSOperationQueue *queue;
+
+@end
 
 @implementation DHDownloadManager
 
@@ -27,6 +44,13 @@
     self = [super init];
     if (self) {
         
+        // 初始化
+        self.imageCache = [NSMutableDictionary dictionary];
+        self.operationCache = [NSMutableDictionary dictionary];
+        self.queue = [[NSOperationQueue alloc]init];
+        
+        // 注册内存警告的通知
+        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(memoryWarning) name:UIApplicationDidReceiveMemoryWarningNotification object:nil];
         
     }
     return self;
@@ -37,6 +61,62 @@
     // 断言
     NSAssert(compeletion != nil, @"必须传入回调的block");
     
+    // 判断操作有没有
+    if (self.operationCache[urlString] != nil) {
+        NSLog(@"操作已经存在,代表正在下载中....");
+        return;
+    }
+    
+    // 创建一个操作去下载图片
+    DHDownloadOperation *op = [DHDownloadOperation operationWithUrlString:urlString];
+    
+    __weak DHDownloadOperation *weakSelf = op;
+    [op setCompletionBlock:^{
+        
+        // 取到图片
+        UIImage *image = op.image;
+        
+        // 回到主线程中调用,将image传出去
+        [[NSOperationQueue mainQueue] addOperationWithBlock:^{
+            
+            // 保存到内存中一份
+            [self.imageCache setObject:image forKey:urlString];
+            // 将操作从缓存中移除
+            [self.operationCache removeObjectForKey:urlString];
+            
+            compeletion(image);
+            
+        }];
+    }];
+    
+    // 将操作添加到缓存中
+    [self.operationCache setObject:op forKey:urlString];
+    
+    // 将操作添加到队列
+    [self.queue addOperation:op];
+    
+    NSLog(@"创建操作下载图片");
+}
+/**
+ *  收到内存警告应该做的事情
+ */
+- (void)memoryWarning {
+    
+    NSLog(@"收到内存警告");
+    // 清除图片缓存
+    [self.imageCache removeAllObjects];
+    // 清除操作缓存
+    [self.operationCache removeAllObjects];
+    // 将队列中所有操作取消
+    [self.queue cancelAllOperations];
+    
+    
+}
+
+- (void)dealloc {
+    
+    //移除通知
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
     
 }
 
